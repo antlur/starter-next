9563625167351ec156166ea67d57455ea17d13c4
Anthony Holmes <anthony@antlur.co>
2024-11-28 00:13:41 -0600

    Change div to main for semantic HTML in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased flex flex-col`}>
         <Header />
-        <div className="flex-grow">{children}</div>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
