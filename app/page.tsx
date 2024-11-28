3ad85b8cf0f8763090acbfdb1e1292bd8da03f7e
Anthony Holmes <anthony@antlur.co>
2024-11-27 19:55:05 -0600

    Add middleware for request handling and logging; include console logs in layout and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("Home Page");
   return (
     <div>
       <div className="max-w-xl">
---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log(new URL(request.url).pathname);
+
+  // if not /, throw 404
+  if (new URL(request.url).pathname !== "/") {
+    console.log("Not found");
+    return new Response("Not found", { status: 404 });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
