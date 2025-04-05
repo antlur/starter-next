046d39141e6a01b1935bc70519a5d4097d21945c
Anthony Holmes <anthony@antlur.co>
2025-04-05 02:58:54 -0500

    chore: add @svgr/webpack as a dev dependency

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,65 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.bckstg.app"],
   },
+
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+
+  async headers() {
+    return [
+      {
+        //cache all images, fonts, etc. in the public folder
+        //Note: Next.js default is 'public, max-age=0' which cases many reloads on Safari!
+        //Note: we use version hashes and therefore can use immutable
+        source: "/:all*(svg|jpg|jpeg|png|gif|ico|webp|mp4|ttf|otf|woff|woff2)",
+
+        headers: [
+          {
+            key: "cache-control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
